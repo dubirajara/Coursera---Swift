b486e2cf1c3f7ff7373d0378d94f563fc0bd0128
Diego Ubirajara <diego.ubirajara@mail.com>
2015-10-20 23:08:16 +0200

    modificacion Mini Reto Coursera Swift 2

--- minireto.playground/Contents.swift
+++ minireto.playground/Contents.swift
@@ -6,19 +6,19 @@ for num in 0...100 {
     
     if num >= 30 && num <= 40 {
         
-        print(num, "#Viva Swift")
+        print("\(num) #Viva Swift")
     }
         
     else if num % 5 == 0 {
-        print(num, "#Bingo!!!")
+        print("\(num) #Bingo!!!")
     }
     else if num % 2 == 0{
-        print(num, "#Par!!!")
+        print("\(num) #Par!!!")
         
     }
         
     else if num % 2 != 0 {
-        print(num, "#Impar!!!")
+        print("\(num) #Impar!!!")
     }
     
     
