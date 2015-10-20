175b0ef8ecc55514880d4a663e62f18b0c8d2c04
Diego Ubirajara <diego.ubirajara@mail.com>
2015-10-20 22:41:00 +0200

    Mini Reto Coursera Swift 2

---
+++ minireto.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for num in 0...100 {
+    
+    if num >= 30 && num <= 40 {
+        
+        print(num, "#Viva Swift")
+    }
+        
+    else if num % 5 == 0 {
+        print(num, "#Bingo!!!")
+    }
+    else if num % 2 == 0{
+        print(num, "#Par!!!")
+        
+    }
+        
+    else if num % 2 != 0 {
+        print(num, "#Impar!!!")
+    }
+    
+    
+}
