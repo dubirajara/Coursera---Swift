2d625d2857f19ee6165c5f62a177083c5e3c031c
Diego Ubirajara <diego.ubirajara@mail.com>
2015-10-23 09:57:29 +0200

    File modificato

--- minireto.playground/Contents.swift
+++ minireto.playground/Contents.swift
@@ -4,22 +4,51 @@ import UIKit
 
 for num in 0...100 {
     
-    if num >= 30 && num <= 40 {
+    var divisible = num % 5 == 0
+    var par = num % 2 == 0
+    var impar = num % 2 != 0
+    var vivaSwift = num >= 30 && num <= 40
+    
         
-        print("\(num) #Viva Swift")
+    if divisible && par && vivaSwift {
+        print("\(num) #Bingo!!! #Viva Swift!!! #Par!!!")
+        
     }
         
-    else if num % 5 == 0 {
-        print("\(num) #Bingo!!!")
+    else if divisible && impar && vivaSwift {
+        print("\(num) #Bingo!!! #Viva Swift!!! #Impar!!!")
+        
     }
-    else if num % 2 == 0{
+    
+        
+    else if par && vivaSwift {
+        print("\(num) #Viva Swift!!! #Par!!!")
+    }
+        
+    else if impar && vivaSwift {
+        print("\(num) #Viva Swift!!! #Impar!!!")
+    }
+    
+    else if par && divisible{
+        print("\(num) #Bingo!!! #Par!!!")
+        
+    }
+        
+    else if par{
         print("\(num) #Par!!!")
         
     }
         
-    else if num % 2 != 0 {
-        print("\(num) #Impar!!!")
+    else if impar && divisible{
+        print("\(num) #Bingo!!! #Impar!!!")
+    
     }
     
-    
+    else if impar{
+        print("\(num) #Impar!!!")
+        
+    }
+
+
 }
+
